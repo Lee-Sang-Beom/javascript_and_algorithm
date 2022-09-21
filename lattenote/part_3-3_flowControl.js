cd7709559843d635793276ad8bc376b56500f675
lattesmoody <lattesmoody@gmail.com>
2022-09-21 23:00:57 +0900

    part3 0~4 complete.

---
+++ lattenote/part_3-3_flowControl.js
@@ -0,0 +1,32 @@
+/*
+
+[흐름 제어]
+
+1. Control Flow
+: 조건이나 반복을 통해 상태를 제어.
+
+예)
+- 조건문: 조건이 맞을 때만 실행되는 문장
+1.1 if: 괄호 안 조건식이 참인 경우 실행되는 문법.
+
+주의해야 할 점: 
+거짓 => false, undefined, null, 0, NaN, ""(empty string)
+
+1.2 switch
+: 괄호 안값에 따라 분기되는 문법
+(case, default)
+
+- 반복문 (초기문; 조건문; 증감문;)
+
+1.3 while : 괄호 안 조건이 거짓이 될 때까지.
+
+1.4 do-while: 먼저 진입, 로직 실행, 다음 조건 검사.
+
+
+2. Data Flow (함수형 프로그래밍)
+예)
+Stateless
+Recursion
+Pipe
+
+*/
